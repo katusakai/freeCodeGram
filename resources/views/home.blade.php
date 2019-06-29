214b91c70bc6f742a485cecf48b09838c31250b4
TadasJanca <tadasjanca@gmail.com>
2019-06-29 15:52:50 +0300

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img class="rounded-circle" src="https://instagram.fkun1-1.fna.fbcdn.net/vp/2933f466753bbf5f9f376eef3ddb98f3/5DB47138/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fkun1-1.fna.fbcdn.net" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freecodecamp</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
         </div>
     </div>
