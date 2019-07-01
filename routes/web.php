4152c0d9ba42256e347eb623262df0d983958042
TadasJanca <tadasjanca@gmail.com>
2019-07-01 08:38:00 +0300

    followed profiles posts feed added

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::post('follow/{user}', 'FollowsController@store');
 
+Route::get('/', 'PostsController@index');
 Route::get('/p/create', 'PostsController@create')->name('post.create');
 Route::get('/p/{post}', 'PostsController@show')->name('post.show');
 Route::post('/p', 'PostsController@store');
